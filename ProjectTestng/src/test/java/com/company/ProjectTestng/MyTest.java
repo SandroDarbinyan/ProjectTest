dc296e64905b470543268435dc399341de09a0a8
SANDRO <SANDRO@WIN-LC35MJVTHGA>
2019-11-27 13:15:25 +0300

    First commit

---
+++ ProjectTestng/src/test/java/com/company/ProjectTestng/MyTest.java
@@ -0,0 +1,12 @@
+package com.company.ProjectTestng;
+
+import org.testng.annotations.Test;
+
+public class MyTest {
+  
+	@Test
+	public void action() {
+		System.out.println("Hello World!!!");
+	}
+	
+}
